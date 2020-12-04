4697daece5c64f2919d54f947d2f686a87ef6c91
David Pecollet <david.pecollet@gmail.com>
2020-12-04 11:31:32 +0000

    svg support
    as png does not seem to be a default terminal type
    plus it looks better

--- metrics/plot_metrics.sh
+++ metrics/plot_metrics.sh
@@ -40,9 +40,9 @@ do
   file_count=$(echo ${fileset} |wc -w)
   echo "    plotting data from ${file_count} file(s)"
   #plot column 2 of each file in the set, for the specified range
-  gnuplot -p -c ${dir}/neo.metrics.gnuplot_png "${fileset}" 2 ${2} ${3}
+  gnuplot -p -c ${dir}/neo.metrics.gnuplot_svg "${fileset}" 2 ${2} ${3}
   #writing the result to a html report (img of the chart + zero-size text for search)
-  echo '<img src="'${f}'.png" border=1><small style="font-size: 0px;">'${f}'</small>' >> ${OUTPUT_FILE}
+  echo '<img src="'${f}'.svg" border=1><small style="font-size: 0px;">'${f}'</small>' >> ${OUTPUT_FILE}
 done
 echo '</BODY></HTML>' >> ${OUTPUT_FILE}
 echo "Done."
